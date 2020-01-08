03ecec7731d1d51cba3e83ec34e9b05bcf73dddd
Juan Carlos Rivera <juan.rivera@mimacom.com>
2020-01-08 14:47:11 +0100

    88. Step 27 - Connecting Currency Exchange Microservice to Eureka

--- limits-service/src/main/java/com/jc/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/jc/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,12 +2,14 @@ package com.jc.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LimitsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LimitsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(LimitsServiceApplication.class, args);
+    }
 }
