e974832c37aa0cfcab4b8afc913f76be4b6ae57f
Juan Carlos Rivera <juan.rivera@mimacom.com>
2020-01-08 16:10:29 +0100

    96. Step 34 - Setting up Zuul API Gateway between microservice invocations

--- currency-conversion-service/src/main/java/com/jc/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/jc/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -9,10 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 //With ribbon you only use the service's name, since the micro instances you want to call from here will be set in application.properties
 //@FeignClient(name = "currency-exchange-service",
 //            url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+//We just installed Zuul, so now we want to call to the other microservice using our Zuul gateway
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    //We just installed Zuul, so now we want to call to the other microservice using our Zuul gateway
+    //@GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionBean getExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
